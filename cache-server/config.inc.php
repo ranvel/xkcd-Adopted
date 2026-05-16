45e82e458008699dde5079d7328f6e41c8287f3c
ran mak <ranvel@ranvel.com>
2026-05-16 14:04:01 -0400

    removed: defunct CI config and unused push notification server

--- cache-server/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('DB_DSN', 'mysql:host=localhost;dbname=xkcdos');
-define('DB_USER', 'xkcdos');
-define('DB_PASSWORD', 'password');
-
-define('APS_CERT_PATH', '/etc/ssl/apns/xkcdos.pem');

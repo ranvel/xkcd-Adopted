edd869d9a78f6bf3e5ade9d78e7ea3a04069b4ad
ran mak <ranvel@ranvel.com>
2026-05-16 13:42:42 -0400

    added: vendor pods for long-term reproducibility
    cocoapods is on life support

---
+++ Pods/Target Support Files/Pods-xkcdOSTests/Pods-xkcdOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xkcdOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_xkcdOSTests
+@end
---
+++ Pods/Target Support Files/Pods-xkcdToday/Pods-xkcdToday-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xkcdToday : NSObject
+@end
+@implementation PodsDummy_Pods_xkcdToday
+@end
